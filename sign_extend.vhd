65a45a2a4a7913c15f35b1de08eb7bb7b649e085
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-04-22 02:33:05 +0200

    Added condition for LDM

--- sign_extend.vhd
+++ sign_extend.vhd
@@ -4,12 +4,20 @@ use IEEE.numeric_std.all;
 
 entity sign_extend is
     port (
+        func : in std_logic_vector(2 downto 0);
         instruction_in: in std_logic_vector(15 downto 0);
         instruction_out: out std_logic_vector(31 downto 0)
     );
 end entity sign_extend;
 
 architecture Behavioral of sign_extend is
-begin
-    instruction_out <= (15 downto 0 => instruction_in(15)) & instruction_in;
-end Behavioral;
+    begin
+        process(instruction_in, func)
+        begin
+            if func = "111" then
+                instruction_out <= "0000000000000000" & instruction_in;
+            else
+                instruction_out <= (15 downto 0 => instruction_in(15)) & instruction_in;
+            end if;
+        end process;
+    end Behavioral;
