d1910c236efd360ddf97816bd35c4e853facd3cc
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-28 14:52:56 +0300

    Made Protection Unit synchronous

--- ProtectionUnit.vhd
+++ ProtectionUnit.vhd
@@ -4,8 +4,7 @@ USE IEEE.numeric_std.ALL;
 
 ENTITY ProtectionUnit IS
     PORT (
-        -- In reset do we need to make all bits unprotected??
-        MemWrite, Protect, Free : IN STD_LOGIC;
+        Clk, Rst, MemWrite, Protect, Free : IN STD_LOGIC;
         Address : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
         MemoryEn, Exception : OUT STD_LOGIC
     );
@@ -14,7 +13,27 @@ END ProtectionUnit;
 ARCHITECTURE struct_ProtectionUnit OF ProtectionUnit IS
     SIGNAL memory : STD_LOGIC_VECTOR(4096 DOWNTO 0) := (OTHERS => '0');
 BEGIN
-    memory(to_integer(unsigned(Address))) <= '1' WHEN (Protect = '1' AND Free = '0') ELSE '0' WHEN (Protect = '0' AND Free = '1') ELSE memory(to_integer(unsigned(Address)));
-    MemoryEn <= '1' WHEN (MemWrite = '1' AND (memory(to_integer(unsigned(Address))) = '0' OR Free = '1')) ELSE '0';
-    Exception <= '1' WHEN (memory(to_integer(unsigned(Address))) = '1' AND MemWrite = '1' ) ELSE '0';
+    PROCESS (Clk, Rst)
+    BEGIN
+        IF Rst = '1' THEN
+            memory <= (OTHERS => '0');
+        ELSIF rising_edge(Clk) THEN
+            IF Protect = '1' AND Free = '0' THEN
+                memory(to_integer(unsigned(Address))) <= '1';
+            ELSIF Protect = '0' AND Free = '1' THEN
+                memory(to_integer(unsigned(Address))) <= '0';
+            END IF;
+        ELSIF falling_edge(Clk) THEN
+            IF MemWrite = '1' AND (memory(to_integer(unsigned(Address))) = '0' OR Free = '1') THEN
+                MemoryEn <= '1';
+            ELSE
+                MemoryEn <= '0';
+            END IF;
+            IF memory(to_integer(unsigned(Address))) = '1' AND MemWrite = '1' THEN
+                Exception <= '1';
+            ELSE
+                Exception <= '0';
+            END IF;
+        END IF;
+    END PROCESS;
 END struct_ProtectionUnit;
