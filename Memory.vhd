5fff34d049c5f8a2a94d2cf2cf0ad8ced4f8295a
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-05-03 00:19:31 +0300

    Integrated SP

--- Memory.vhd
+++ Memory.vhd
@@ -12,7 +12,9 @@ ENTITY Memory IS
         --    RDest : in STD_LOGIC_VECTOR (2 downto 0);
         Controls : IN STD_LOGIC_VECTOR (9 DOWNTO 0);
         PC : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-        -- Int , stack pointer
+        INT : IN STD_LOGIC;
+        StackPointer : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        INT_FSM : IN STD_LOGIC;
 
         Exception : OUT STD_LOGIC;
         Mem_out : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
@@ -37,8 +39,37 @@ ARCHITECTURE Memory_arch OF Memory IS
         );
     END COMPONENT;
 
+    COMPONENT Mux2 IS
+        GENERIC (n : INTEGER := 32);
+        PORT (
+            selector : IN STD_LOGIC;
+            input1, input2 : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+            output : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    COMPONENT Mux4 IS
+        GENERIC (n : INTEGER := 32);
+        PORT (
+            selectors : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+            input1, input2, input3, input4 : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+            output : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0)
+        );
+    END COMPONENT;
+
     SIGNAL MemWriteEn : STD_LOGIC := '0';
+    SIGNAL address : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    SIGNAL data_in_to_memory : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL mux4_selector : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL extended_flags : STD_LOGIC_VECTOR(31 DOWNTO 0);
+
 BEGIN
-    Memory1 : data_memory PORT MAP(write_enable => MemWriteEn, read_enable => Controls(9), rst => reset, clk => clk, address => ALU_result(11 DOWNTO 0), data_in => RS2, data_out => Mem_out);
-    ProtectionUnit1 : ProtectionUnit PORT MAP(Clk => clk, Rst => reset, MemWrite => Controls(8), Protect => Controls(6), Free => Controls(5), Address => ALU_result(11 DOWNTO 0), MemoryEn => MemWriteEn, Exception => Exception);
+    mux4_selector <= Controls(7) & INT_FSM;
+    extended_flags <= "0000000000000000000000000000" & Flags;
+
+    Mux2_1 : Mux2 GENERIC MAP(12) PORT MAP(selector => Controls(2), input1 => ALU_result(11 DOWNTO 0), input2 => StackPointer(11 DOWNTO 0), output => address);
+    Mux4_1 : Mux4 GENERIC MAP(32) PORT MAP(selectors => mux4_selector, input1 => RS2, input2 => RS2, input3 => extended_flags, input4 => PC, output => data_in_to_memory);
+
+    Memory1 : data_memory PORT MAP(write_enable => MemWriteEn, read_enable => Controls(9), rst => reset, clk => clk, address => address, data_in => data_in_to_memory, data_out => Mem_out);
+    ProtectionUnit1 : ProtectionUnit PORT MAP(Clk => clk, Rst => reset, MemWrite => Controls(8), Protect => Controls(6), Free => Controls(5), Address => address, MemoryEn => MemWriteEn, Exception => Exception);
 END Memory_arch;
--- SP.vhd
+++ SP.vhd
@@ -18,27 +18,34 @@ END SP;
 ARCHITECTURE sp_arch OF SP IS
     SIGNAL push_address : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL pop_address : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL nextStackPointer : STD_LOGIC_VECTOR(31 DOWNTO 0) := (11 DOWNTO 0 => '1', OTHERS => '0');
+    -- SIGNAL nextStackPointer : STD_LOGIC_VECTOR(31 DOWNTO 0) := (11 DOWNTO 0 => '1', OTHERS => '0');
 
 BEGIN
 
     PROCESS (clk, rst, func, int, memRead, loadUse, addressSelector)
     BEGIN
         IF rst = '1' THEN
-            nextStackPointer <= (11 DOWNTO 0 => '1', OTHERS => '0');
+            -- nextStackPointer <= (11 DOWNTO 0 => '1', OTHERS => '0');
+            push_address <= (11 DOWNTO 0 => '1', OTHERS => '0');
         ELSIF falling_edge(clk) THEN
             IF addressSelector = '1' AND loadUse = '0' THEN
-                IF func(2) = '1' AND func(1) = '0' THEN
-                    pop_address <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) + 1);
-                    nextStackPointer <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) + 1);
+                IF func(2) = '1' AND func(1) = '0' AND int = '0' THEN
+                    -- pop_address <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) + 1);
+                    -- nextStackPointer <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) + 1);
+                    stackPointer <= pop_address;
+                    pop_address <= STD_LOGIC_VECTOR(unsigned(pop_address) + 2);
+                    push_address <= STD_LOGIC_VECTOR(unsigned(push_address) + 2);
                 ELSE
-                    push_address <= nextStackPointer;
-                    nextStackPointer <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) - 1);
+                    -- push_address <= nextStackPointer;
+                    -- nextStackPointer <= STD_LOGIC_VECTOR(unsigned(nextStackPointer) - 1);
+                    pop_address <= push_address;
+                    stackPointer <= push_address;
+                    push_address <= STD_LOGIC_VECTOR(unsigned(push_address) - 2);
                 END IF;
             END IF;
         END IF;
     END PROCESS;
 
-    stackPointer <= push_address WHEN memRead = '0' ELSE
-        pop_address;
+    -- stackPointer <= push_address WHEN memRead = '0' ELSE
+    --     pop_address;
 END sp_arch;
