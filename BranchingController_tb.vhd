72670c40365bce9e663ec99fabc6b16f3c1409f2
MalekElsaka <malek.fahmy02@eng-st.cu.edu.eg>
2024-05-13 18:43:24 +0300

    Branching controller final draft hopefully

--- BranchingController_tb.vhd
+++ BranchingController_tb.vhd
@@ -1,70 +1,153 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-ENTITY BranchingController_tb IS
-END BranchingController_tb;
+ENTITY branching_controller_tb IS
+END branching_controller_tb;
 
-ARCHITECTURE behavior OF BranchingController_tb IS 
+ARCHITECTURE behavior OF branching_controller_tb IS
 
-    COMPONENT BranchingController
-    PORT(
-         Decode_Branch : IN  std_logic;
-         Decode_Conditional : IN  std_logic;
-         Execute_Branch : IN  std_logic;
-         Execute_Conditional : IN  std_logic;
-         Can_Branch : IN  std_logic;
-         Zero_Flag : IN  std_logic;
-         Branched_In_Decode : IN  std_logic;
-         Prediction : OUT  std_logic;
-         Branch : OUT  std_logic;
-         Selectors : OUT  std_logic_vector(1 downto 0);
-         Branching_In_Decode : OUT  std_logic
-        );
-    END COMPONENT;
+   COMPONENT branching_controller
+      PORT (
+         a_branch_instruction_is_in_decode : IN STD_LOGIC;
+         a_branch_instruction_is_in_execute : IN STD_LOGIC;
+         decode_branch_conditional : IN STD_LOGIC;
+         execute_branch_conditional : IN STD_LOGIC;
+         branched_in_decode : IN STD_LOGIC;
+         can_branch : IN STD_LOGIC;
+         zero_flag : IN STD_LOGIC;
 
+         prediction_out : OUT STD_LOGIC;
+         two_bit_PC_selector : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+         will_branch_in_decode : OUT STD_LOGIC;
+         branch_out : OUT STD_LOGIC
+      );
+   END COMPONENT;
 
-   signal Decode_Branch : std_logic := '0';
-   signal Decode_Conditional : std_logic := '0';
-   signal Execute_Branch : std_logic := '0';
-   signal Execute_Conditional : std_logic := '0';
-   signal Can_Branch : std_logic := '0';
-   signal Zero_Flag : std_logic := '0';
-   signal Branched_In_Decode : std_logic := '0';
+   --Inputs
+   SIGNAL a_branch_instruction_is_in_decode : STD_LOGIC := '0';
+   SIGNAL a_branch_instruction_is_in_execute : STD_LOGIC := '0';
+   SIGNAL decode_branch_conditional : STD_LOGIC := '0';
+   SIGNAL execute_branch_conditional : STD_LOGIC := '0';
+   SIGNAL branched_in_decode : STD_LOGIC := '0';
+   SIGNAL can_branch : STD_LOGIC := '0';
+   SIGNAL zero_flag : STD_LOGIC := '0';
 
-
-   signal Prediction : std_logic;
-   signal Branch : std_logic;
-   signal Selectors : std_logic_vector(1 downto 0);
-   signal Branching_In_Decode : std_logic;
+   --Outputs
+   SIGNAL prediction_out : STD_LOGIC;
+   SIGNAL two_bit_PC_selector : STD_LOGIC_VECTOR(1 DOWNTO 0);
+   SIGNAL will_branch_in_decode : STD_LOGIC;
+   SIGNAL branch_out : STD_LOGIC;
 
 BEGIN
 
+   uut : branching_controller PORT MAP(
+      a_branch_instruction_is_in_decode => a_branch_instruction_is_in_decode,
+      a_branch_instruction_is_in_execute => a_branch_instruction_is_in_execute,
+      decode_branch_conditional => decode_branch_conditional,
+      execute_branch_conditional => execute_branch_conditional,
+      branched_in_decode => branched_in_decode,
+      can_branch => can_branch,
+      zero_flag => zero_flag,
+      prediction_out => prediction_out,
+      two_bit_PC_selector => two_bit_PC_selector,
+      will_branch_in_decode => will_branch_in_decode,
+      branch_out => branch_out
+   );
+   stim_proc : PROCESS
+   BEGIN
 
-   uut: BranchingController PORT MAP (
-          Decode_Branch => Decode_Branch,
-          Decode_Conditional => Decode_Conditional,
-          Execute_Branch => Execute_Branch,
-          Execute_Conditional => Execute_Conditional,
-          Can_Branch => Can_Branch,
-          Zero_Flag => Zero_Flag,
-          Branched_In_Decode => Branched_In_Decode,
-          Prediction => Prediction,
-          Branch => Branch,
-          Selectors => Selectors,
-          Branching_In_Decode => Branching_In_Decode
-        );
+      WAIT FOR 100 ns;
+      -- Test Case - 1 ************************************************************************************************
+
+      -- This is the case where i am in decode and predicting not taken while there is no data hazard with a conditional branch
+      --prediction bit here is zero by default
+      branched_in_decode <= '0';
+      a_branch_instruction_is_in_decode <= '1';
+      decode_branch_conditional <= '1';
+      can_branch <= '1';
+
+      assert will_branch_in_decode = '0' report "Test case 1, will_branch_in_execute is not '0'" severity error;
+      assert two_bit_PC_selector = "11" report "Test case 1, two_bit_PC_selector_signal is not '11'" severity error;
+      assert prediction_out = '0' report "Test case 1, prediction_out is not '0'" severity error;
+      WAIT FOR 100 ns;
+
+      -- Now the branch instruction will be in the execute, and i predicted correct (not taken)
+      --prediction bit here is zero by default
+      branched_in_decode <= '0';
+      a_branch_instruction_is_in_execute <= '1';
+      execute_branch_conditional <= '1';
+      zero_flag <= '0';
+
+      assert will_branch_in_execute = '0' report "Test case 1, will_branch_in_execute is not '0'" severity error;
+      assert two_bit_PC_selector = "11" report "Test case 1, two_bit_PC_selector_signal is not '11'" severity error;
+      assert prediction_out = '0' report "Test case 1, prediction_out is not '0'" severity error;
+      WAIT FOR 100 ns;
+
+      --******************************************************************************************************************
 
 
-   stim_proc: process
-   begin		
-     
-      wait for 100 ns;	
 
-   
 
-      
+      -- Test Case - 2 ****************************************************************************************************
 
-      wait;
-   end process;
+      -- This is the case where i am in decode and predicting not taken while there is no data hazard with a conditional branch
+      --prediction bit here is zero by default
+      branched_in_decode <= '0';
+      a_branch_instruction_is_in_decode <= '1';
+      decode_branch_conditional <= '1';
+      can_branch <= '1';
+
+      assert will_branch_in_decode = '0' report "Test case 2, will_branch_in_execute is not '0'" severity error;
+      assert two_bit_PC_selector = "11" report "Test case 2, two_bit_PC_selector_signal is not '11'" severity error;
+      assert prediction_out = '0' report "Test case 2, prediction_out is not '0'" severity error;
+      WAIT FOR 100 ns;
+      -- Now the branch instruction will be in the execute, and i predicted wrong (taken)
+      --prediction bit here is zero by default
+      branched_in_decode <= '0';
+      a_branch_instruction_is_in_execute <= '1';
+      execute_branch_conditional <= '1';
+      zero_flag <= '1';
+
+      assert will_branch_in_execute = '1' report "Test case 2, will_branch_in_execute is not '0'" severity error;
+      assert two_bit_PC_selector = "10" report "Test case 2, two_bit_PC_selector_signal is not '11'" severity error;
+      assert prediction_out = '1' report "Test case 2, prediction_out is not '0'" severity error;
+      WAIT FOR 100 ns;
+
+      --*******************************************************************************************************************
+
+
+
+
+
+
+      -- branched_in_decode <= '0';
+      -- a_branch_instruction_is_in_decode <= '1';
+      -- decode_branch_conditional <= '1';
+      -- can_branch <= '0';
+
+      -- -- will_branch_in_decode <= '0';
+      -- -- two_bit_PC_selector_signal <= "11";
+      -- -- prediction_out <= '0';
+      -- -- was_there_a_data_hazard_in_decode <= '1';
+      -- WAIT FOR 100 ns;
+
+      -- branched_in_decode <= '0';
+      -- a_branch_instruction_is_in_decode <= '1';
+      -- decode_branch_conditional <= '0';
+      -- can_branch <= '1';
+
+      -- -- will_branch_in_decode <= '1';
+      -- -- two_bit_PC_selector_signal <= "00";
+
+      -- WAIT FOR 100 ns;
+
+      -- branched_in_decode <= '0';
+      -- a_branch_instruction_is_in_decode <= '1';
+      -- decode_branch_conditional <= '0';
+      -- can_branch <= '0';
+      -- -- will_branch_in_decode <= '0';
+
+      WAIT;
+   END PROCESS;
 
 END;
--- myBranchingController.vhd
+++ myBranchingController.vhd
@@ -5,11 +5,12 @@ ENTITY branching_controller IS
     PORT (
         a_branch_instruction_is_in_decode : IN STD_LOGIC; -- 1 if a branch instruction is in decode stage, 0 otherwise
         a_branch_instruction_is_in_execute : IN STD_LOGIC; -- 1 if a branch instruction is in execute stage, 0 otherwise
-        decode_branch_conditional : IN STD_LOGIC; -- 1 if the instruction in decode stage is a conditional branch, 0 otherwise
-        execute_branch_conditional : IN STD_LOGIC; -- 1 if the instruction in execute stage is a conditional branch, 0 otherwise
+        decode_branch_unconditional : IN STD_LOGIC; -- 1 if the instruction in decode stage is a conditional branch, 0 otherwise
+        execute_branch_unconditional : IN STD_LOGIC; -- 1 if the instruction in execute stage is a conditional branch, 0 otherwise
         branched_in_decode : IN STD_LOGIC; -- 1 if i already have branched in decode stage, 0 otherwise
         can_branch : IN STD_LOGIC; -- 1 if the branch can be taken in decode stage, 0 otherwise
         zero_flag : IN STD_LOGIC; -- 1 if the zero flag is set after execute stage, 0 otherwise
+        any_stall : IN STD_LOGIC; -- 1 if there is a stall in the pipeline, 0 otherwise
         prediction_out : OUT STD_LOGIC; -- 1 bit prediction of the branch instruction in decode stage, can be 0 or 1 and it toggles
         two_bit_PC_selector : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- 2 bit selector for the PC, 00 for decode branch update, 01 for execute branch update, 10 for register address, 11 for normal PC update +2
         will_branch_in_decode : OUT STD_LOGIC; -- 1 if the branch will be taken in decode stage, 0 otherwise
@@ -17,82 +18,81 @@ ENTITY branching_controller IS
     );
 END branching_controller;
 
--- remaining: line 49 vs 123 should it be execute branch update or wrong prediction
--- line 93 in other file, there is a decode case inside the branched_in_decode condition, why?, it is written that its for interrupts
-
 ARCHITECTURE branching_controller_arch OF branching_controller IS
     SIGNAL prediction_bit : STD_LOGIC := '0';
-    SIGNAL two_bit_PC_selector_signal : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL two_bit_PC_selector_signal : STD_LOGIC_VECTOR(1 DOWNTO 0) := "11";
     SIGNAL was_there_a_data_hazard_in_decode : STD_LOGIC := '0';
 BEGIN
-    PROCESS (a_branch_instruction_is_in_decode, a_branch_instruction_is_in_execute, decode_branch_conditional, execute_branch_conditional, can_branch, zero_flag, branched_in_decode)
+    PROCESS (a_branch_instruction_is_in_decode, a_branch_instruction_is_in_execute, decode_branch_unconditional, execute_branch_unconditional, can_branch, zero_flag, branched_in_decode)
         VARIABLE will_branch_in_execute : STD_LOGIC := '0';
     BEGIN
-        IF (branched_in_decode = '1') THEN
-            IF (a_branch_instruction_is_in_execute = '1') THEN
-                IF (execute_branch_conditional = '1') THEN
-                    IF (prediction_bit /= zero_flag) THEN
-                        will_branch_in_execute := '1';
-                        two_bit_PC_selector_signal <= "10";
+        IF (any_stall = '0') THEN
+            IF (branched_in_decode = '1') THEN
+                IF (a_branch_instruction_is_in_execute = '1') THEN
+                    IF (execute_branch_unconditional = '0') THEN
+                        IF (prediction_bit /= zero_flag) THEN
+                            will_branch_in_execute := '1';
+                            two_bit_PC_selector_signal <= "10";
+                        ELSE
+                            prediction_bit <= '0';
+                            will_branch_in_execute := '0';
+                            two_bit_PC_selector_signal <= "11";
+                        END IF;
                     ELSE
-                        prediction_bit <= '0';
                         will_branch_in_execute := '0';
                         two_bit_PC_selector_signal <= "11";
+                    END IF;
+                END IF;
+            ELSE
+                IF (a_branch_instruction_is_in_execute = '1') THEN
+                    IF (execute_branch_unconditional = '0') THEN
+                        IF (zero_flag = '1') THEN
+                            IF (was_there_a_data_hazard_in_decode = '1') THEN
+                                two_bit_PC_selector_signal <= "01"; -- Execute branch update
+                            ELSE
+                                prediction_bit <= '1';
+                                two_bit_PC_selector_signal <= "10"; -- Wrong prediction
+                            END IF;
+                            will_branch_in_execute := '1';
+                        ELSE
+                            will_branch_in_execute := '0';
+                            two_bit_PC_selector_signal <= "11";
+                        END IF;
+                    ELSE
+                        will_branch_in_execute := '1';
+                        two_bit_PC_selector_signal <= "01";
                     END IF;
                 ELSE
                     will_branch_in_execute := '0';
+                END IF;
+
+                IF (a_branch_instruction_is_in_decode = '1' AND will_branch_in_execute = '0') THEN
+                    IF (decode_branch_unconditional = '0') THEN
+                        IF (prediction_bit = '1' AND can_branch = '1') THEN
+                            will_branch_in_decode <= '1';
+                            two_bit_PC_selector_signal <= "00";
+                        ELSE
+                            IF (can_branch = '0') THEN
+                                was_there_a_data_hazard_in_decode <= '1';
+                            END IF;
+                            will_branch_in_decode <= '0';
+                            two_bit_PC_selector_signal <= "11";
+                        END IF;
+                    ELSE
+                        IF (can_branch = '1') THEN
+                            will_branch_in_decode <= '1';
+                            two_bit_PC_selector_signal <= "00";
+                        ELSE
+                            will_branch_in_decode <= '0';
+                        END IF;
+                    END IF;
+                ELSIF will_branch_in_execute = '0' THEN
                     two_bit_PC_selector_signal <= "11";
                 END IF;
             END IF;
-        ELSE
-            IF (a_branch_instruction_is_in_execute = '1') THEN
-                IF (execute_branch_conditional = '1') THEN
-                    IF (zero_flag = '1') THEN
-                        IF (was_there_a_data_hazard_in_decode = '1') THEN
-                            two_bit_PC_selector_signal <= "01"; -- Execute branch update
-                        ELSE
-                            prediction_bit <= '1';
-                            two_bit_PC_selector_signal <= "10"; -- Wrong prediction
-                        END IF;
-                        will_branch_in_execute := '1';
-                    ELSE
-                        will_branch_in_execute := '0';
-                        two_bit_PC_selector_signal <= "11";
-                    END IF;
-                ELSE
-                    will_branch_in_execute := '1';
-                    two_bit_PC_selector_signal <= "01";
-                END IF;
-            ELSE
-                will_branch_in_execute := '0';
-            END IF;
-
-            IF (a_branch_instruction_is_in_decode = '1' AND will_branch_in_execute = '0') THEN
-                IF (decode_branch_conditional = '1') THEN
-                    IF (prediction_bit = '1' AND can_branch = '1') THEN
-                        will_branch_in_decode <= '1';
-                        two_bit_PC_selector_signal <= "00";
-                    ELSE
-                        IF (can_branch = '0') THEN
-                            was_there_a_data_hazard_in_decode <= '1';
-                        END IF;
-                        will_branch_in_decode <= '0';
-                        two_bit_PC_selector_signal <= "11";
-                    END IF;
-                ELSE
-                    IF (can_branch = '1') THEN
-                        will_branch_in_decode <= '1';
-                        two_bit_PC_selector_signal <= "00";
-                    ELSE
-                        will_branch_in_decode <= '0';
-                    END IF;
-                END IF;
-            ELSIF will_branch_in_execute = '0' THEN
-                two_bit_PC_selector_signal <= "11";
-            END IF;
         END IF;
-        prediction_out <= prediction_bit;
-        branch_out <= two_bit_PC_selector_signal(0) NAND two_bit_PC_selector_signal(1);
-        two_bit_PC_selector <= two_bit_PC_selector_signal;
     END PROCESS;
+    prediction_out <= prediction_bit;
+    branch_out <= two_bit_PC_selector_signal(0) NAND two_bit_PC_selector_signal(1);
+    two_bit_PC_selector <= two_bit_PC_selector_signal;
 END branching_controller_arch;
